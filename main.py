4dd1e4a807d89fd4de342e9adf7ff8b545f6daa5
kyy <insider_2012@mail.ru>
2022-12-26 01:25:54 +0300

    Parser School Bookz

---
+++ main.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+from pathlib import Path
+import requests
+import urllib.request
+import shutil
+
+
+
+
+def parse(urls):
+    r = requests.get(urls)
+    r = r.content
+    soup = BeautifulSoup(r, 'html.parser')
+    return soup
+
+url = 'https://gdz-shok.ru' # 0-вая страница сайта
+
+all_links = parse(url+'/gdz/') 
+all_links = all_links.find_all('a') # все ссылки с 0-вой страницы (нужные + мусор)
+
+base ={} # получаем полезный словарь -  {название книги: ссылка на страницы}
+
+for link in all_links:
+    text = link.text
+    link = url+link.get('href')
+    if 'ГДЗ,' in  text: # отсеиваем полезную информацию
+        base.update({
+        text: link
+        })       
+
+for key in base:
+    pages = parse(base[key])
+    pages = pages.select('.content ul a')
+    links = [] # получаем страницы книги 
+    split = base[key].split('/')
+    file = Path(f'books/{split[4]}/{split[5]}/{key}')
+    file.mkdir(exist_ok=True, parents=True)
+    for link in pages:
+        link = link.get('href').replace('/','')
+        links.append(link)
+    for i in links:
+        img = parse(base[key]+i)
+        img = img.select('.ex img')
+        for link in img:
+            src = url+link.get('src')  # ссылка на картинку
+            img = urllib.request.urlopen(src).read()
+            with open(f'{i}.jpg', "wb") as f_obj:
+                f_obj.write(img)
+                f_obj.close
+            target_folder = Path.cwd() / file
+            source_folder = Path.cwd() / f'{i}.jpg'
+            shutil.move(source_folder, target_folder)
+          
+     
+    
+
+
+
+
+
+
+
+
+
+
+
