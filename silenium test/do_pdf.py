bd6323053367b5349ae288f4c02fb7df4600838a
kyy <insider_2012@mail.ru>
2023-01-10 08:40:12 +0300

    logging

--- silenium test/do_pdf.py
+++ silenium test/do_pdf.py
@@ -7,5 +7,5 @@ def do_pdf(dict_, name):
     pdf.set_font('Times', '', 12)
     pdf.set_title('Cars')
     for key in dict_:
-        pdf.image(name=dict_[key], x=None, y=None, w=190, h=0, type='', link=key)
+        pdf.image(name=dict_[key], w=190, h=0, type='', link=key)
     return pdf.output(f'{name}.pdf')
