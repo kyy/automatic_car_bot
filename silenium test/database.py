c1ec0d00c2e425a136e93f37273bc01371d71491
kyy <insider_2012@mail.ru>
2023-01-30 11:36:41 +0300

    db create tabeles

--- silenium test/database.py
+++ silenium test/database.py
@@ -1,24 +1,39 @@
+import asyncio
 import aiosqlite
 
+
 source = 'https://aiosqlite.omnilib.dev/en/stable/'
-conn = await aiosqlite.connect('auto_db')
-cur = conn.cursor()
 
 
-cur.execute("""CREATE TABLE brands (
-    id       INTEGER      PRIMARY KEY AUTOINCREMENT,
-    [unique] TEXT (0, 32) UNIQUE
-                          NOT NULL,
-    av_by    TEXT (0, 32) UNIQUE,
-    abw_by   TEXT (0, 32) UNIQUE
-)""")
+async def create_tables(db):
+    try:
+        await db.execute("""
+        CREATE TABLE brands(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            [unique] TEXT (0, 32) UNIQUE NOT NULL,
+            av_by TEXT (0, 32) UNIQUE,
+            abw_by TEXT (0, 32) UNIQUE
+            )""")
 
-cur.execute("""CREATE TABLE models (
-    id       INTEGER      PRIMARY KEY AUTOINCREMENT,
-    brand_id INTEGER      REFERENCES brands (id) ON DELETE CASCADE,
-    [unique] TEXT (0, 32) UNIQUE
-                          NOT NULL,
-    av_by    TEXT (0, 32) UNIQUE,
-    abw_by   TEXT (0, 32) UNIQUE
-);
-)""")
+        await db.execute("""
+        CREATE TABLE models(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            brand_id INTEGER REFERENCES brands (id) ON DELETE CASCADE,
+            [unique] TEXT (0, 32) UNIQUE NOT NULL,
+            av_by TEXT (0, 32) UNIQUE,
+            abw_by TEXT (0, 32) UNIQUE
+            )""")
+        await db.commit()
+        print('Таблицы - brands, models успешно созданы')
+    except Exception as e:
+        print(e, 'Таблицы уже существуют')
+
+
+async def main():
+    async with aiosqlite.connect('auto_db') as db:
+        await asyncio.gather(create_tables(db),
+                             )
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
