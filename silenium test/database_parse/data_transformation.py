a82d0d8f2a9aeae8e40676ed278b17c98e0c3639
kyy <insider_2012@mail.ru>
2023-02-16 16:38:51 +0300

    av_brands update

--- silenium test/database_parse/data_transformation.py
+++ silenium test/database_parse/data_transformation.py
@@ -3,12 +3,12 @@ import aiosqlite
 import numpy as np
 from tqdm import tqdm
 
-def to_tuple(dictionary):
+
+def br_to_tuple(dictionary: dict[str: [str, str]]) -> list[(str, str)]:
     new = {}
-    for key, value in dictionary.items():
-        new.update({key: value[0]})
-    new = list(new.items())
-    return new
+    [new.update({key: value[0]}) for key, value in dictionary.items()]
+    return list(new.items())
+
 
 abw_m = np.load('abw_models.npy', allow_pickle=True).item()
 av_m = np.load('av_models.npy', allow_pickle=True).item()
@@ -16,10 +16,7 @@ onliner_m = np.load('onliner_models.npy', allow_pickle=True).item()
 abw_b = np.load('abw_brands.npy', allow_pickle=True).item()
 av_b = np.load('av_brands.npy', allow_pickle=True).item()
 onliner_b = np.load('onliner_brands.npy', allow_pickle=True).item()
-
-
-
-source = 'https://aiosqlite.omnilib.dev/en/stable/'
+l_av_b = len(av_b)
 
 
 async def create_tables(db):
@@ -48,16 +45,32 @@ async def create_tables(db):
         print(e, '\nТаблицы уже существуют')
 
 
-# async def av_brands(db):
-#     await db.executemany("INSERT INTO brands([unique], av_by) VALUES(?, ?);", ---)
-#     await db.commit()
+async def av_brands(db):
+    cursor = await db.execute(f"select [unique], id  from brands ")
+    av_bd = await cursor.fetchall()
+    l_av_bd = len(av_bd)
+    if l_av_bd == 0:
+        await db.executemany("""INSERT INTO brands([unique], av_by) VALUES(?, ?)""", br_to_tuple(av_b))
+        await db.commit()
+    else:
+        print(f'Кол-во брендов: БД/av.by  {l_av_bd}/{l_av_b}')
+        update = []
+        for item in tqdm(av_bd):
+            if item[0] in av_b:
+                update.append((item[1], item[0], av_b[item[0]][0]))
+        await db.executemany("""REPLACE INTO brands(id, [unique], av_by) VALUES(?, ?, ?)""", update)
+        await db.commit()
+        print(f'Данные обновлены brands([unique], av_by)')
+        if l_av_b != l_av_bd:
+            print(f'Добавлено - {l_av_b-l_av_bd}')
+
 
 
 async def main():
     async with aiosqlite.connect('test_db') as db:
         await asyncio.gather(
             #create_tables(db),
-            #av_brands(db),
+            av_brands(db),
         )
 
 
