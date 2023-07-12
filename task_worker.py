239fb18c0210c4a63861f5ef1d16c4468847d7c2
xibolba <insider_2102@mail.ru>
2023-07-12 16:38:16 +0300

    arq test

---
+++ task_worker.py
@@ -0,0 +1,36 @@
+import asyncio
+from httpx import AsyncClient
+from arq import create_pool
+from arq.connections import RedisSettings
+
+from b_logic.get_url_cooking import all_get_url
+from b_logic.parse_cooking import parse_main
+
+
+async def parse(ctx, car):
+    av_link_json, abw_link_json, onliner_link_json = await all_get_url(car)
+    result = parse_main(av_link_json, abw_link_json, onliner_link_json, message='test', name='01')
+    return print(result)
+
+
+async def startup(ctx):
+    ctx['session'] = AsyncClient()
+
+async def shutdown(ctx):
+    await ctx['session'].aclose()
+
+async def main():
+    redis = await create_pool(RedisSettings())
+    for car in ('Abarth+?+?+?+?+?+?+?+?+?', 'Audi+A2+b+?+?+?+?+?+?+?', 'Audi+A2+dg+a+?+?+?+?+?+?'):
+        await redis.enqueue_job('parse', car)
+
+# WorkerSettings defines the settings to use when creating the work,
+# it's used by the arq cli.
+# For a list of available settings, see https://arq-docs.helpmanual.io/#arq.worker.Worker
+class WorkerSettings:
+    functions = [parse]
+    on_startup = startup
+    on_shutdown = shutdown
+
+if __name__ == '__main__':
+    asyncio.run(main())
