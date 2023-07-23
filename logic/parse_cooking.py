dea574ece0ec876ebad9898b2313b2afce3258cb
kyy <insider_2012@mail.ru>
2023-07-23 21:08:39 +0300

    work optimize

--- logic/parse_cooking.py
+++ logic/parse_cooking.py
@@ -2,12 +2,9 @@ import asyncio
 import numpy as np
 from aiohttp import ClientSession
 import nest_asyncio
-from arq import create_pool
-from arq.connections import RedisSettings
 from .parse_sites.av_by import json_links_av, bound_fetch_av
 from .parse_sites.abw_by import json_links_abw, bound_fetch_abw
 from .parse_sites.onliner_by import json_links_onliner, bound_fetch_onliner
-from classes import bot
 
 
 nest_asyncio.apply()
@@ -40,14 +37,7 @@ async def run(urls_av, urls_abw, urls_onliner, result, work):
         await asyncio.gather(*tasks)
 
 
-# async def send_car(tel_id, url):
-#     try:
-#         await bot.send_message(tel_id, url)
-#     except Exception as e:
-#         print(e)
-
-
-async def parse_main(url_av, url_abw, url_onliner, message, name, work):
+async def parse_main(url_av, url_abw, url_onliner, message, name, work, send_car_job):
     """
     :param url_av: ссылка к json файлу
     :param url_abw: ссылка к json файлу
@@ -62,13 +52,11 @@ async def parse_main(url_av, url_abw, url_onliner, message, name, work):
     future = asyncio.ensure_future(run(json_links_av(url_av),
                                        json_links_abw(url_abw),
                                        json_links_onliner(url_onliner),
-                                       result, work,
+                                       result, work
                                        ))
     loop.run_until_complete(future)
     if work is True:
-        redis = await create_pool(RedisSettings())
-        for car in result:
-            await redis.enqueue_job('send_car', message, car[0])
+        await send_car_job(message, result)   # отправляемв в очередь задания на отправку обяъвлений
     else:
         np.save(f'logic/buffer/{name}.npy', result)
     return result
--- work.py
+++ work.py
@@ -1,7 +1,6 @@
 import asyncio
 from arq import create_pool, cron
 from arq.connections import RedisSettings
-
 from classes import bot
 from logic.database.config import database
 from logic.database.data_migrations import main as update, lenn
@@ -12,22 +11,19 @@ from logic.parse_cooking import parse_main
 
 async def parse(ctx, car, message, name, work):
     av_link_json, abw_link_json, onliner_link_json = await all_get_url(car, work)
-    await parse_main(av_link_json, abw_link_json, onliner_link_json, message, name, work)
+    await parse_main(av_link_json, abw_link_json, onliner_link_json, message, name, work, send_car_job)
 
 
 async def send_car(ctx, tel_id, url):
-    try:
-        await bot.send_message(tel_id, url)
-    except Exception as e:
-        print(e)
+    await bot.send_message(tel_id, url)
 
 
-async def base(ctx):
+async def update_database(ctx):
     if main_parse(lenn):
         asyncio.run(update(database()))
 
 
-async def collect_data(ctx):
+async def collect_data_job(ctx):
     redis = await create_pool(RedisSettings())
     async with database() as db:
         select_filters_cursor = await db.execute(f"""
@@ -40,15 +36,21 @@ async def collect_data(ctx):
             await redis.enqueue_job('parse', item[1][7:], item[0], item[2], True)
 
 
+async def send_car_job(message, result):
+    redis = await create_pool(RedisSettings())
+    for car in result:
+        await redis.enqueue_job('send_car', message, car[0])
+
+
 class Work:
     functions = [parse, send_car]
     cron_jobs = [
-        cron(collect_data,
+        cron(collect_data_job,
              hour={i for i in range(1, 24)},
              minute={00, 30},
-             run_at_startup=False,),   # парсинг новых объявлений
-        cron(base,
+             run_at_startup=True, ),   # парсинг новых объявлений
+        cron(update_database,
              hour={00}, minute={15},
              max_tries=3,
-             run_at_startup=True),  # обновление БД
+             run_at_startup=False),  # обновление БД
     ]
