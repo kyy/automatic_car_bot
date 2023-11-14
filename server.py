71a98fe864c1b95101aa589a76ef0fc12706dbe1
xibolba <insider_2102@mail.ru>
2023-11-14 16:39:43 +0300

    try server

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from aiohttp import web
 from webhook import webhook
 import aiohttp_jinja2
 import jinja2
+import aiohttp_debugtoolbar
+
 
 WEB_SERVER_HOST = "127.0.0.1"
 WEB_SERVER_PORT = 8350
@@ -13,15 +15,20 @@ routes = web.RouteTableDef()
 @routes.get('/')
 @aiohttp_jinja2.template('index.html')
 async def index(request):
-    return {'title': 'automatic-car-bot', 'surname': 'Svetlov'}
+    return {
+        'bot_url': 'https://t.me/AutomaticCarBot',
+        'bot_name': 'automatic-car-bot',
+        'app': request.app,
+    }
 
 
 app = web.Application()
-
-aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('server'))
+app.router.add_static('/static/', path='server/static', name='static')
+app.add_routes(routes)
 app['static_root_url'] = 'server/static'
 
-app.add_routes(routes)
+aiohttp_debugtoolbar.setup(app)
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('server/templates'))
 
 webhook(app)
 
@@ -31,4 +38,3 @@ if __name__ == '__main__':
         web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
     except (KeyboardInterrupt, SystemExit):
         print("Server stopped")
-
