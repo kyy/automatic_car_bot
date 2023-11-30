221ea7645477dd7f97ed0c490c919266654268ea
xibolba <insider_2102@mail.ru>
2023-11-30 13:58:22 +0300

    fix numpy

--- 1stupdate.py
+++ 1stupdate.py
@@ -2,6 +2,7 @@ import asyncio
 from logic.database.config import database
 from logic.database.data_migrations import main as migration
 from sites.sites_get_update import get_parse_brands_models as parsing
+from logic.fix_num import *
 
 if __name__ == "__main__":
     asyncio.run(parsing())
