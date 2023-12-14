b44302a24b3dbfb2cd1393eca7782e62659899a7
xibolba <insider_2102@mail.ru>
2023-12-14 12:22:20 +0300

    redis cashecardata

--- logic/image_comparison.py
+++ logic/image_comparison.py
@@ -14,47 +14,49 @@ def check_pictures(pic1, pic2):
     return ImageChops.difference(pic_1, pic_2).getbbox()
 
 
-redis = redis.Redis(host='127.0.0.1', port=6379, db=10)
+r = redis.Redis(host='127.0.0.1', port=6379, db=10)
 
 
-class DublicateRomover:
+class CacheCarData:
     def __init__(self, tel_id, max_len_data=3):
-        self.queue = []
         self.max_len_data = max_len_data  # число используемых элементов
         self.tel_id = str(tel_id)  # id телеграм пользователя
-        redis.hset(self.tel_id + '_count', 'count', '0')
+        self.s = '_|_'
+        self.data_name = self.tel_id + self.s
+        self.count_name = self.tel_id + self.s + 'count'
 
-    def get_count(self):
-        'строка счетчика'
-        get_count = redis.hget(self.tel_id + '_count', 'count').decode('utf-8')
-        return int(get_count)
+    def count(self):
+        try:
+            count = int(r.get(self.count_name))
+        except TypeError:
+            count = 0
+        count = 0 if count is None or count >= self.max_len_data else count
+        return count
 
-    def get_data(self):
-        cur_id, data = redis.sscan(self.tel_id + '_data')
+    def push(self, val: list):
+        count = self.count()
+        count += 1
+        r.set(self.count_name, count)
+        print(count)
+        return r.set(self.data_name + str(count), self.s.join(str(i) for i in val))
+
+    def render(self):
+        data = []
+        for i in range(1, self.max_len_data + 1):
+            item = r.get(self.tel_id + self.s + str(i)).decode('UTF-8').split(self.s)
+            data.append(item)
         return data
 
-    def delete_data(self):
 
-        redis.hset(self.tel_id + '_count', 'count', '0')
-
-    def push(self, val):
-        if self.get_count() <= self.max_len_data:
-            count = self.get_count()
-            count += 1
-            redis.sadd(self.tel_id + '_data', str(count), *val)
-            redis.hset(self.tel_id + '_count', 'count', str(count))
-        else:
-            redis.delete(self.tel_id + '_data', '1')
-            redis.sadd(self.tel_id + '_data', '1', *val)
-
-    def export(self):
-        pass
-
-
-rem = DublicateRomover(123456)
+rem = CacheCarData(123456)
 
 if __name__ == '__main__':
-    while True:
-        for i in range(1, 100):
-            pass
-            time.sleep(1)
+    print(rem.render())
+    # while True:
+    #     for i in range(100):
+    #         rem.push([f'{i}_http:/www.av.by/1232341', f'{i}_http:/image.png/', f'{i}_115546'])
+    #
+    #         print('1:', r.get('123456_|_1'))
+    #         print('2:', r.get('123456_|_2'))
+    #         print('3:', r.get('123456_|_3'))
+    #         time.sleep(0.35)
