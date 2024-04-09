aaa3a0a7671c5ebd05ad17c060093e36293896e9
xibolba <insider_2102@mail.ru>
2024-04-09 10:00:34 +0300

    change headers_json (fix av resp)

--- logic/constant.py
+++ logic/constant.py
@@ -89,7 +89,14 @@ HEADERS = {
     "accept": "*/*",
 }
 
-HEADERS_JSON = HEADERS.update({"content-type": "application/json"})
+HEADERS_JSON = (
+    {
+        "Accept-Encoding": "gzip, deflate, br",
+        "accept": "	text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+    }
+)
 
 DEFAULT = dict(
     chosen_brand=SB,
